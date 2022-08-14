c55c9a9369c4a021a9d64945767666943bfd9fdf
magenligne <magenligne@outlook.fr>
2022-08-14 12:36:29 +0200

    implémentation de la page produit bouton "Ajouter au panier" compris

--- front/js/index.js
+++ front/js/index.js
@@ -1,70 +1,53 @@
 // ce fichier permet de récupérer les données sur les canapés dans l'api,
 // les traite pour les convertir en lien a html dans la classe items de index.html,
-//  ce qui permet de les afficher dans la page acceuil (via StyleSheet.css). 
-
+//  ce qui permet de les afficher dans la page acceuil (via Style.css).
 
 // d'abord on récupère la liste de données de l'api au format json en faisant une requete grace à fetch,
 // et cette requette on la met dans une variable pour l'utiliser comme promesse.
 let myPromise = fetch("http://localhost:3000/api/products");
 
-// comment utiliser cela?:
-// myPromise((resolve,reject)=>{
-//     let responseFromServer;
-//     if(responseFromServer){
-//     resolve("Données récupérées");
-//     } else {
-//     reject("Echec de récupération des données");
-//     }
-//     }  );
 
-    myPromise
-    .then(function (result){
-            // à l'endroit de result se trouve le résultat de la promesse 
-            // (ici de notre fetch) au format Texte . Pour l'utiliser en JS,
-            // il faut convertir ce résultat au format JSON en faisant result.json(), si le result existe .
+myPromise
+  .then(function (result) {
+    // à l'endroit de result se trouve le résultat de la promesse
+    // (ici de notre fetch) au format Texte . Pour l'utiliser en JS,
+    // il faut convertir ce résultat au format JSON en faisant result.json(), si le result existe .
 
-        if(result){
-    return result.json();
-    }})
-    .then(listProductJson=>{
-      // à l'endroit de listProductJson se trouve le résultat du then précédent,
-      // ici result.json qui est la liste des canapés au format json, et qu'on nomme 
-      // donc listProductJson.
-        // console.log(listProductJson);
-        for (let ProductJson of listProductJson) {
-            // console.log(ProductJson);
-// pour chaque objet json de la liste on crée un objet js canape rempli à l'aide du constructeur
-            let canapeLocal = new canape(ProductJson);
-            // console.log(canapeLocal);
-            // document.querySelector(".items") récupère dans le dom le contenu de la classe "items"
-            // ensuite avec la méthode .innerHTML on remplace ce contenu html par ce qui suit le =.
-            // enfin grâce au += on concatène ce qui suit avec ce qui existe déjà au lien de remplacer.
-            document.querySelector(".items").innerHTML+=` <a href="./product.html?id=${canapeLocal._id}">
+    if (result) {
+      return result.json();
+    }
+  })
+  .then((listProductJson) => {
+    // à l'endroit de listProductJson se trouve le résultat du then précédent,
+    // ici result.json qui est la liste des canapés au format json, et qu'on nomme
+    // donc listProductJson.
+    // console.log(listProductJson);
+    for (let ProductJson of listProductJson) {
+      // console.log(ProductJson);
+      // pour chaque objet json de la liste on crée un objet js canape rempli à l'aide du constructeur
+      let canapeLocal = new canape(ProductJson);
+      // console.log(canapeLocal);
+      // document.querySelector(".items") récupère dans le dom le contenu de la classe "items"
+      // ensuite avec la méthode .innerHTML on remplace ce contenu html par ce qui suit le =.
+      // enfin grâce au += on concatène ce qui suit avec ce qui existe déjà au lien de remplacer.
+      document.querySelector(
+        ".items"
+      ).innerHTML += ` <a href="./product.html?id=${canapeLocal._id}">
             <article>
               <img src=${canapeLocal.imageUrl} alt=${canapeLocal.altTxt}>
               <h3 class="productName">"${canapeLocal.name}"</h3>
               <p class="productDescription">${canapeLocal.description}</p>
             </article>
-          </a> `
-        }
-        // let listItems=(document.querySelectorAll(".items a"));
-        // console.log(listItems);
-        // return listItems;
-        })
+          </a> `;
+    }
+    // let listItems=(document.querySelectorAll(".items a"));
+    // console.log(listItems);
+    // return listItems;
+  })
 
-        // le backtick pour délimiter une chaîne permet 1/de faire du multiligne et
-        // 2/d'utiliser des ${} pour passer des variables dans la chaîne.
+  // le backtick pour délimiter une chaîne permet 1/de faire du multiligne et
+  // 2/d'utiliser des ${} pour passer des variables dans la chaîne.
 
-    .catch(error=>{
+  .catch((error) => {
     console.log(error);
-    });
-
-   
-
-
-    
-
-
-
-
-  
+  });
--- front/js/listenIndex.js
+++ front/js/listenIndex.js
@@ -1,17 +1,66 @@
 // ce fichier permet d'inspecter l'url de la page produit quand on click sur les liens des canapés de la page acceuil.
 // cet url contient l'id du canapé cliqué qu'on récupère pour construire et afficher
-// la page produit correspondante. 
+// la page produit correspondante.
+// let canapId;
+window.addEventListener("load", () => {
+  // alert("Page chargée")//test chargement ok
+  // console.log(window.location);//test:affiche l'url de la page
+  //nous allons créer un object de type URLSearchParams qui permet de récupérer des paramètres d'URL et lui passer en argument la chaine à droite du ?
+  let searchId = new URLSearchParams(window.location.search); //URL.search correspond à la chaine de caractère de l'url à droite du ?
+  // console.log(searchId.has('id'));//test:renvoie true si il y a id dans la chaine à droite de ? dans l'url de la page produit.
+  if (searchId.has("id")) {
+    let canapId = searchId.get("id");
 
-window.addEventListener('load',()=>{
-    // alert("Page chargée")//test chargement ok
-    // console.log(window.location);//test:affiche l'url de la page
-    //nous allons créer un object de type URLSearchParams qui permet de récupérer des paramètres d'URL et lui passer en argument la chaine à droite du ?
-    let searchId=new URLSearchParams(window.location.search);//URL.search correspond à la chaine de caractère de l'url à droite du ?
-    // console.log(searchId.has('id'));//test:renvoie true si il y a id dans la chaine à droite de ? dans l'url de la page produit.
-    if(searchId.has('id')){
-let canapId=searchId.get('id');
+    // console.log(canapId);
+
+    let myPromise = fetch("http://localhost:3000/api/products/" + canapId); //le résultat de la promesse contient l'article voulu au format txt API
+    myPromise
+      .then(function (result) {
+        if (result) {
+          return result.json();
+        }
+      })
+      .then((ProductJson) => {//le résultat du then précédent (article au format json) étant lui-même une promesse, on lui applique un then à lui aussi
+        // console.log(ProductJson);
+        // console.log(ProductJson.name);
+        //traitement des couleurs:
+        for(let color of ProductJson.colors)//pour chaque item du tableaux des couleurs de l'article, on ajoute cette couleurs aux autres dans le html de l'article.
+        {
+            // console.log(color);
+            document.querySelector("#colors").innerHTML += ` <option value="${color}">${color}</option> `
+
+        }
+        //ajout des autres propriété de l'article dans le html de la page produit:
+        document.querySelector(".item__img").innerHTML = `<img src=${ProductJson.imageUrl}>`;
+        document.querySelector("#title").innerText = ProductJson.name;
+        document.querySelector("#price").innerText = ProductJson.price;
+        document.querySelector("#description").innerText =
+          ProductJson.description;
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
+//comment sortir les variables searchId et canapId pour éviter la répèt suivante?
+let searchId = new URLSearchParams(window.location.search); //URL.search correspond à la chaine de caractère de l'url à droite du ?
+
+let canapId = searchId.get("id");
+
 console.log(canapId);
-    }else{
-        window.location.pathname='index.html';
-    }
+localStorage.Id=canapId;
+
+let ajoutPanier = document.getElementById('addToCart');//on recupere le pointage du bouton dans le DOM dans une variable
+ajoutPanier.addEventListener('click',() =>{
+        // console.log('ajout au panier');
+    //récup de la quantité:
+    let quantity=document.getElementById("quantity").value;
+    localStorage.quantite=quantity;
+//récup de la couleur:
+    let color=document.getElementById("colors").value;
+    localStorage.couleur=color;
+    // console.log(color);  
+window.location="cart.html";
 },false);
+
