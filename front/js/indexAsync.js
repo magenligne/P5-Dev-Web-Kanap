2eea92afc8536744d54c4fae5476e3262b81ce65
magenligne <magenligne@outlook.fr>
2022-08-31 09:57:23 +0200

    utilisation de await dans indexAsync.js à la place de la promesse dans index.js

---
+++ front/js/indexAsync.js
@@ -0,0 +1,20 @@
+let reponse =await fetch("http://localhost:3000/api/products")
+
+
+let listProductJson=await reponse.json();
+    
+  
+    for (let ProductJson of listProductJson) {
+      let canapeLocal = new canape(ProductJson);
+      document.querySelector(
+        ".items"
+      ).innerHTML += ` <a href="./product.html?id=${canapeLocal._id}">
+            <article>
+              <img src=${canapeLocal.imageUrl} alt=${canapeLocal.altTxt}>
+              <h3 class="productName">${canapeLocal.name}</h3>
+              <p class="productDescription">${canapeLocal.description}</p>
+            </article>
+          </a> `;
+    }
+  
+    
